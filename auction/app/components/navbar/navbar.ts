8241c407a52f45b5d905cec65be53052b68bb274
Sanjit Verma <SANJIT@192-168-1-7.tpgi.com.au>
2018-08-19 18:52:44 +0530

    auction project basic set-up

---
+++ auction/app/components/navbar/navbar.ts
@@ -0,0 +1,8 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'auction-navbar',
+  templateUrl: 'app/components/navbar/navbar.html'
+})
+export default class NavbarComponent {}
