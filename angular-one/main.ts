57aa10dfd7de30310ce1485628039417ade5cabf
Sanjit Verma <SANJIT@192-168-1-7.tpgi.com.au>
2018-08-19 10:24:35 +0530

    angular tutorials

---
+++ angular-one/main.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {Directive, ElementRef, Renderer} from '@angular/core';
+// Component
+@Component({
+  selector: 'hello-world',
+  template: `<h1>Hello {{ name }}!</h1>`
+})
+class HelloWorldComponent {
+  name: string;
+
+  constructor() {
+    this.name = 'Sanjit';
+  }
+}
+
+@Directive({
+    selector: 'input[log-directive]',
+    host: {
+        '(input)': 'onInput($event)'
+    }    
+})
+export class LogDirective{
+    onInput(event){
+         console.log(event.target.value);   
+    }
+}
+
+
+// Module
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ HelloWorldComponent, LogDirective ],
+  bootstrap:    [ HelloWorldComponent ]
+})
+export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
