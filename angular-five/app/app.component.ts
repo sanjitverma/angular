164baf771cee511e2c9c6dafcf18f1b1c07c86dd
Sanjit Verma <verma.sanjit@gmail.com>
2018-10-23 17:32:14 +0530

    "reactive capability"

--- angular-five/app/app.component.ts
+++ angular-five/app/app.component.ts
@@ -1,26 +1,32 @@
 import { NgModule,Component } from '@angular/core';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import "rxjs/add/operator/debounceTime";
 
 
 @Component({
     selector: 'two-way',
     template: `
-    <input type="text" [(ngModel)]="inputValue"/>
-    <p> The value of inputValue variable is {{ inputValue }}</p>    
+    <input type="text" placeholder="Enter your stock here" [formControl]="searchStock">
+    <p> The stock price of {{ searchStock.value }} is  {{ stockPrice }}</p>    
     `
 
 })
 export class TwoWayComponent{
 
-    inputValue: string;
+    searchStock: FormControl = new FormControl("");
+    stockPrice: number;
     constructor(){
-
-        setTimeout(() => {
-            this.inputValue = "AAPL";
-        }, 2000)
-
+        this.searchStock
+            .valueChanges
+            .debounceTime(500)
+            .subscribe(stock => this.getStockPrice(stock));
     }
 
+    getStockPrice(stock: string): void {
+        this.stockPrice = 100 * Math.random();
+    }
 }
+
 
 @Component({
     selector: 'app',
--- angular-weather/app/app.module.ts
+++ angular-weather/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppComponent, TwoWayComponent} from './app.component';
 import { ReactiveFormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
-    imports: [BrowserModule, ReactiveFormsModule],
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule],
     declarations: [AppComponent,TwoWayComponent],
     bootstrap: [AppComponent]
 })
