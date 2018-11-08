8ce428d1dbc06c9df09ca0f1a2d1451754fb795f
Sanjit Verma <verma.sanjit@gmail.com>
2018-11-08 17:33:03 +0530

    Added basic web server

--- angular-seven-template-form/app/app.component.ts
+++ angular-seven-template-form/app/app.component.ts
@@ -1,13 +1,20 @@
 import {Component} from '@angular/core';
 import "rxjs/add/operator/debounceTime";
-import {FormGroup, FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule} from "@angular/forms";
+
+function ssnValidator(control: FormControl): {[key: string]: any} {
+    const value: string = control.value || '';
+    const valid = value.match(/^\d{9}$/);
+    return valid ? null : {ssn: true};
+}
 
 @Component({
     selector: 'app',
     template: `
         <div>
             <form #frm="ngForm"  (ngSubmit)="onFormSubit(frm.value)" >
-            <div> UserName : <input type="text" name="username" ngModel/></div>
+            <div> UserName : <input type="text" name="username" ngModel/>
+            </div>
             <div>SSN : <input type="text" name="ssn" ngModel/></div>
             <div ngModelGroup="password">
                 <div>Password: <input type="password" name="mpasswd" ngModel/></div>
